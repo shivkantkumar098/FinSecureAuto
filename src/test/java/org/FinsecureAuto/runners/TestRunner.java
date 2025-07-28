7a1c3f00f77d1eeac8dc3d1d497f5257821d9c08
shivkant-epam <shivkantkr9934@gmail.com>
2025-07-28 09:29:44 +0530

    Test Runner file created with Allure Plugin

---
+++ src/test/java/org/FinsecureAuto/runners/TestRunner.java
@@ -0,0 +1,28 @@
+package org.FinsecureAuto.runners;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"org.finSecure.stepDefinitions", "org.finSecure.hooks"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber.html",
+                "json:target/cucumber-reports/cucumber.json",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        },
+        monochrome = true,
+        dryRun = false
+//        tags = "@smoke"
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+    // Enables parallel execution if needed
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
+
